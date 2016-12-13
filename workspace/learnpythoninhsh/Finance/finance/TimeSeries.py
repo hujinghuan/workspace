5102c59ffc414101307239c36475b6149bee4c7a
hujinghuan <2850211598@qq.com>
2016-12-13 17:56:19 +0800

    2016-12-13-3

--- workspace/learnpythoninhsh/Finance/finance/TimeSeries.py
+++ workspace/learnpythoninhsh/Finance/finance/TimeSeries.py
@@ -46,19 +46,26 @@ print(groups.size())
 df['Odd_Even']=['Odd','Even','Odd','Even','Odd','Even','Odd','Even','Odd']
 groups=df.groupby(['Quarter','Odd_Even'])
 print(groups.size())
-'''
+
 DAX=web.DataReader(name='GOOG', data_source='yahoo', start='2015-1-1', end='2015-12-31')
 DAX['42d']=Series.rolling(DAX['Close'],window=42).mean()
 
 DAX[['Close','42d']].plot(figsize=(8,5))
 plt.show()
-'''
+
 DAX['Ret_Loop']=0.0
 for i in range(1,len(DAX)):
     DAX['Ret_Loop'][i]=np.log(DAX['Close'][i]/DAX['Close'][i-1])
     
 print(DAX[['Close','Ret_Loop']])
-'''
+
 DAX['Return']=np.log(DAX['Close']/DAX['Close'].shift(1))
 print(DAX[['Close','Return']])
+'''
 
+df=pd.read_csv('usd-wti.csv')
+df.index=df['date']
+
+print(df[['usd','wti']])
+
+print(df[['usd','wti']].corr())
